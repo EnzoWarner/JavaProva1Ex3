c53fbda9dbd072c2c136c98a4a1bb0f0f344f73d
EnzoWarner <enzo_warner@hotmail.com>
2024-05-22 16:26:52 -0300

    first commit

---
+++ prova.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class prova {
+public static void main (String []args){
+         Scanner sc = new Scanner (System.in);
+                 Locale.setDefault(Locale.US);
+  for (int i = 0; i <= 20; i ++) {
+        while (i > 8);
+        int c = 0;
+        c = c + i;
+        System.out.println("Os numeros maiores que 8 são: " + c);
+   }
+  sc.close();
+}
+}
